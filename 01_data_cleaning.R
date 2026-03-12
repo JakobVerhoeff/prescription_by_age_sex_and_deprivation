07bac9cb83fc0143293a598f1ca2da0b8a124729
AbelSK <abelkjaersgaard@gmail.com>
2026-03-12 12:09:59 +0000

    readme and quintiles in cleaning

--- 01_data_cleaning.R
+++ 01_data_cleaning.R
@@ -16,11 +16,11 @@ age_band_levels <- c(
 
 # Read FOI data for all years in directory
 foi_path <- "data/foi"
-foi_files <- list.files(path = foi_path, full.names = TRUE)[3] # 3 hard coded here. Remove
+foi_files <- list.files(path = foi_path, full.names = TRUE) # 3 hard coded here. Remove
 
-foi_raw <- foi_files %>%
-  map_df(~ read_csv(.x, show_col_types = FALSE)) %>%
-  clean_names() %>%
+foi_raw <- foi_files |>
+  map_df(~ read_csv(.x, show_col_types = FALSE)) |>
+  clean_names() |>
   mutate(
     gender = str_to_lower(gender),
     age_band = str_remove(age_band, "Age "),
@@ -56,33 +56,46 @@ imd_data <- imd_raw |>
       sum(population, na.rm = TRUE),
     population = sum(population, na.rm = TRUE),
     .by = lad_code
+  ) |>
+  mutate(
+    imd_quintile = ntile(imd_score, 5),
+    imd_quintile = paste0("Q", 6 - imd_quintile)
   )
 
 # 2. Load reference lookup tables ---------------------------------------
 ## Practice-postcode lookup table ---------------------------------------
-gp_to_postcode <- read_csv("data/epraccur-2.csv", show_col_types = FALSE) %>%
+gp_to_postcode <- read_csv("data/epraccur-2.csv", show_col_types = FALSE) |>
   select(practice_code, postcode)
 
 ## Postcode-LAD lookup table ---------------------------------------
 postcode_lookup <- read_csv(
   "data/PCD_OA21_LSOA21_MSOA21_LAD_FEB24_UK_LU.csv",
   show_col_types = FALSE
-) %>%
-  select(pcd7, pcd8, lad_code = ladcd)
+) |>
+  select(pcd7, pcd8, pcds, lad_code = ladcd)
 
 # 3. Geographic joining ---------------------------------------
 foi_combined <- foi_data |>
   left_join(gp_to_postcode, by = "practice_code") |>
+  # Join 1: pcd7
   left_join(
     postcode_lookup |> select(pcd7, lad_code),
     by = c("postcode" = "pcd7")
   ) |>
+  # Join 2: pcd8
   left_join(
     postcode_lookup |> select(pcd8, lad_code),
     by = c("postcode" = "pcd8"),
     suffix = c("_7", "_8")
   ) |>
-  mutate(lad_code = coalesce(lad_code_7, lad_code_8)) |>
+  # Join 3: pcds
+  left_join(
+    postcode_lookup |> select(pcds, lad_code),
+    by = c("postcode" = "pcds")
+  ) |>
+  # Coalesce all three attempts (renaming the third join to match)
+  mutate(lad_code = coalesce(lad_code_7, lad_code_8, lad_code)) |>
+  # Clean up temporary join columns
   select(-lad_code_7, -lad_code_8) |>
   reframe(
     total_patients = sum(unique_patient_count, na.rm = TRUE),
@@ -90,12 +103,12 @@ foi_combined <- foi_data |>
     .by = c(bnf_chemical_substance_code, gender, age_band, lad_code)
   )
 
-# 6. FINAL MERGE --------------------------------------------------------------
-combined_data <- foi_combined %>%
+# 4. Final data ---------------------------------------
+combined_data <- foi_combined |>
   left_join(imd_data, by = "lad_code")
 
-# Final Cleanup
 rm(list = setdiff(ls(), "combined_data"))
 
 # Preview result
-combined_data
+combined_data |>
+  head()
--- README.md
+++ README.md
@@ -1,12 +1,44 @@
-Write up here: //
+# Prescription by Age, Sex and Deprivation
 
 Authors: Jakob Verhoeff, Naomi Waterlow, Gwen Knight
+## Overview
 
-*Data* 
-Prescription data: Available as an FOI request here: https://opendata.nhsbsa.net/dataset/foi-02243/resource/e45ebfda-a8af-43b2-a74e-f4436eadbc6f
+This repository contains the analysis and figure-generation code for: "title" (link).
+* 01_data_cleaning.R: prepares the data for analysis
+* 1_figure1.R: generates ...
+* 2_binomial.R: etc ...
 
-<<<<<<< HEAD
-Data on GP practices populations: Available from NHS Digital. We chose to use the April 2022 dataset to match the start of the finanical year of our data. https://digital.nhs.uk/data-and-information/publications/statistical/patients-registered-at-a-gp-practice/april-2022
+## Data sources
 
-Data on IMD2019 deprivation by GP practice
-https://fingertips.phe.org.uk/search/deprivation#page/9/gid/1/pat/159/par/K02000001/ati/15/are/E92000001/iid/93553/age/1/sex/4/cat/-1/ctp/-1/yrr/1/cid/4/tbm/1
+All datasets used in this analysis are publicly available and can be accessed via the following links:
+
+- FOI prescription data (NHSBSA):
+	https://opendata.nhsbsa.net/dataset/foi-02243/resource/e45ebfda-a8af-43b2-a74e-f4436eadbc6f
+- IMD data (NHS Digital CSV downloads):
+    https://www.gov.uk/government/statistics/english-indices-of-deprivation-2019
+- Geography lookup table (GP practice code to postcode lookup):
+	https://digital.nhs.uk/services/organisation-data-service/data-search-and-export/csv-downloads/gp-and-gp-practice-related-data
+- Geography lookup table (Postcode to LAD lookup):
+	https://geoportal.statistics.gov.uk/datasets/f9c8996d451f44b79ab97ddd369ad5db/about
+
+Please note the specific filenames and formats expected by the cleaning script (see 01_data_cleaning.R). Please note the expected `data/` folder structure for storing raw and intermediate files:
+
+data/
+├── foi/
+│   └── (FOI raw data files) (e.g., foi02243_practice_2022_2023.csv)
+├── (IMD data) (e.g., File_7_-_All_IoD2019_Scores__Ranks__Deciles_and_Population_Denominators_3.csv)
+├── (Postcode-LAD lookup table)  (e.g., PCD_OA21_LSOA21_MSOA21_LAD_FEB24_UK_LU.csv)
+└── (GP-postcode lookup table) (e.g., epraccur-2.csv)
+
+## Reproducing the analysis
+
+R version and other requirements.
+Analysis execution instructions.
+
+## Notes
+
+Notes here.
+
+## License & Contact
+
+Licence and contact statement.
